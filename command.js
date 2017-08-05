3627668c435e94a8f86c097211f4aa300cb23c4f
The-Complex <willculhane3@gmail.com>
2017-08-04 22:07:03 -0700

    Added fs requirement

--- command.js
+++ command.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const fs = require('fs');
 
 const config = require('config.json')('./secrets.json');
 const token = config.token;
