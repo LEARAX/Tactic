a47a99f9a343db642c5a4fe1489a154e3d1f38a7
LEARAX57H <willculhane3@gmail.com>
2018-01-08 13:05:02 -0700

    Minor change

--- tictactoe.js
+++ tictactoe.js
@@ -54,11 +54,9 @@ function botMove (gameBoard) {
 
 function checkWin (lastMove, gameBoard) {
   /*
-   * let matrixGameBoard = [
-   *  [ gameboard[0], gameboard[1], gameboard[2] ],
-   *  [ gameboard[3], gameboard[4], gameboard[5] ],
+   *  [ gameboard[0], gameboard[1], gameboard[2] ]
+   *  [ gameboard[3], gameboard[4], gameboard[5] ]
    *  [ gameboard[6], gameboard[7], gameboard[8] ]
-   * ]
    */
   var solution = ''
 
